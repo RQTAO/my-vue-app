6c1586aaef6d55b376b91a1d4248b423a61cde54
RQTAO <ROTAOEQ@163.com>
2024-08-22 13:20:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 // import ElementPlus from 'unplugin-element-plus/vite'
-export default defineConfig(({command,mode}){
-  const env = loadEnv(mode,process.cwd(),'');
-    console.log(import.meta.env,process.cwd());
-    return {
-        base:'/JSlearn.github.io/',
-        // clearScreen:true,
-        // envPrefix:"APP_"
-        server:{
-            // port:3000,
-            // strictPort:true
-            open:"index.html"
-        },
-        // logLevel:"silent",
-        envDir:'dirname'
-    }
+export default defineConfig({
   plugins: [
     vue(),
     // ElementPlus(),
